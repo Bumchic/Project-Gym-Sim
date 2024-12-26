2b51c58e77eac5902163f633454a4656aba64a81
DoThanhLoc2334 <locdothanh233@gmail.com>
2024-12-26 20:57:21 +0700

    Add Start Menu

---
+++ ProjectGymSim/ProjectGymSim/MenuView/StartMenu.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ProjectGymSim.MenuView;
+
+public partial class StartMenu : UserControl
+{
+    public StartMenu()
+    {
+        InitializeComponent();
+    }
+}
