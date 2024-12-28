9e7eec48abc8caa023a01e06779b52ca517d1dc5
Bumchic <duonganhminh05@gmail.com>
2024-12-28 22:08:20 +0700

    Fixed the BarTimer

--- ProjectGymSim/ProjectGymSim/GamePartView/GameView.axaml.cs
+++ ProjectGymSim/ProjectGymSim/GamePartView/GameView.axaml.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Threading;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using System.Threading.Tasks;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Avalonia.Media.Imaging;
 
 namespace ProjectGymSim.GamePartView;
 
@@ -13,8 +18,12 @@ public partial class GameView : Window
     public double Progress { get; set; }
     private const double UpperLim = 100.0;
     private const double LowerLim = 0.0;
-    public Action decrease;
+    private double PressProgress;
     public int Difficulty{get;set;}
+    List<Image> GameFrame = new List<Image>();
+    private Task BarTimer;
+    private Task AddProgress;
+    
     public GameView()
     {
         InitializeComponent();
@@ -22,35 +31,38 @@ public partial class GameView : Window
 
     public GameView(int difficulty) : this()
     {
+
         this.Difficulty = difficulty;
         Progress = 50;
-        this.ProgressBar.Value = Progress;
- 
-    }
-
-    public void Button_OnClick(object? sender, RoutedEventArgs e)
-    {
-        Task.Run(DecreaseProgress);
-    }
-
-    public void DecreaseProgress()
-    {
-        while (this.Progress > LowerLim)
+        PressProgress = 0;
+        BarTimer = new Task(() =>
         {
-            Progress = Progress - 1;
-            this.ProgressBar.Value = Progress;
-            Console.WriteLine(Progress);
-            Task.Delay(1000).Wait();
-        }
-    }
-    public async void SpaceButton_OnKeyDown(object? sender, KeyEventArgs e)
-    {
-        if (e.Key != Key.Space)
+            while (this.Progress > LowerLim)
+            {
+                Progress--;
+
+                Console.WriteLine(Progress);
+                Task.Delay(100).Wait();
+            }
+        });
+        
+        BarTimer.Start();
+        /*for (int i = 0; i < 2; i++)
         {
-            Console.WriteLine("Space");
-            return;
-        }
-        this.Progress += 3;
-        this.ProgressBar.Value = Progress;
+            using Stream stream = File.Open($"/ProjectGymSim/Assets/Frame{i}.png", FileMode.Open);
+            Image img = new Image()
+            {
+                Source = new Bitmap(stream),
+                IsVisible = false
+            };
+            GameFrame.Add(img);
+        }*/
+
+    }
+    
+    public void SpaceButton_OnKeyDown(object? sender, KeyEventArgs e)
+    {
+        Progress += 3;
+
     }
 }
