dca8124476919be5782b8fdee5e9fb10861bed41
DoThanhLoc2334 <locdothanh233@gmail.com>
2024-12-30 21:48:30 +0700

    Nothing

--- ProjectGymSim/ProjectGymSim/obj/Debug/net8.0/ProjectGymSim.AssemblyInfo.cs
+++ ProjectGymSim/ProjectGymSim/obj/Debug/net8.0/ProjectGymSim.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectGymSim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+704bb53b7bce0a7418105339909e1beeeb8d7829")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a33ef82e376bc557838ff44d020f170c49aec52")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectGymSim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectGymSim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
